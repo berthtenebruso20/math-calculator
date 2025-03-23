9fb1a03981787ed9a4f33c2a27e00d3175907727
berthtenebruso20 <berthtenebruso20@gmail.com>
2025-03-23 11:17:01 +0000

    Add 18.js

---
+++ src/18.js
@@ -0,0 +1,6 @@
+// Calculate the sum of two numbers a and b using JavaScript's built-in Math module
+function calculateSum(a, b) {
+    return (a + b);
+}
+
+console.log(calculateSum(3, 5)); // Output: 8
