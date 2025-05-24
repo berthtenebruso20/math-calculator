768e91701b26148eced0e5cbf33cc3e0bfe775fe
berthtenebruso20 <berthtenebruso20@gmail.com>
2025-05-24 16:28:23 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,21 @@
+console.log("Welcome to Math Calculator!");
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 !== 0) {
+    return num1 / num2;
+  } else {
+    console.log("Error: Division by zero is not allowed.");
+    return undefined;
+  }
+}
