307dbaa6865dfdee8239022a9d43814d99a3815b
berthtenebruso20 <berthtenebruso20@gmail.com>
2025-05-26 21:34:01 +0000

    Add 42.js

---
+++ src/42.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if (y === 0) throw new Error('Divide by zero error');
+  return x / y;
+}
+
+console.log(add(10, 5)); // Output: 15
+console.log(subtract(10, 5)); // Output: 5
+console.log(multiply(10, 5)); // Output: 50
+console.log(divide(10, 5)); // Output: 2.0
